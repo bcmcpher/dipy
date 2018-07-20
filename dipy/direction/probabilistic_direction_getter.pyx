e40528bb41e061910324b508f3e45f011d4eff5e
bcmcpher <bcmcpher@iu.edu>
2018-07-20 17:12:12 -0400

    use the right import after cleaning installation dir out

--- dipy/direction/probabilistic_direction_getter.pyx
+++ dipy/direction/probabilistic_direction_getter.pyx
@@ -17,7 +17,7 @@ from dipy.direction.peaks import peak_directions, default_sphere
 from dipy.direction.pmf cimport PmfGen, SimplePmfGen, SHCoeffPmfGen
 from dipy.utils.fast_numpy cimport cumsum, where_to_insert
 
-from dipy.tracking.local.interpolation import trilinear_interpolate4d
+from dipy.tracking.local.interpolation cimport trilinear_interpolate4d_c
 
 cdef class ProbabilisticDirectionGetter(PmfGenDirectionGetter):
     """Randomly samples direction of a sphere based on probability mass
@@ -125,7 +125,7 @@ cdef class ProbabilisticDirectionGetter(PmfGenDirectionGetter):
         _len = pmf.shape[0]
 
         ## interpolate cos_mat max angle at point
-        val = trilinear_iterpolate4d(cos_mat2, point)
+        z = trilinear_iterpolate4d_c(cos_mat2, point, val)
         
         ## find max cosine similarity from precomputed angle array
         ## point has to go from mm to ijk? - _map_to_voxel / _to_voxel_coordinates
